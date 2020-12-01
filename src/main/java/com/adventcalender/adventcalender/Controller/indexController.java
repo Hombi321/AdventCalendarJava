0e76fb6e826a2bb853ec3214d505960fb1bbd80c
Hombi321 <Cyril.homberger@bluewin.ch>
2020-12-01 09:04:07 +0100

    updated

--- src/main/java/com/adventcalender/adventcalender/Controller/indexController.java
+++ src/main/java/com/adventcalender/adventcalender/Controller/indexController.java
@@ -1,11 +1,17 @@
 package com.adventcalender.adventcalender.Controller;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:3000", "https://adventskalender-hombi321.herokuapp.com:443/"})
 public class indexController {
+
+
+
+
     @GetMapping("/")
     public String index() {
         return "Hello there! I'm running.";
